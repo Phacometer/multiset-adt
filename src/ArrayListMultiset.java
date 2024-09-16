9dcdb12ae919f8ba0146b1826eae55ca82d8d08b
Victor Gao <victorgao44@gmail.com>
2024-09-16 14:25:24 -0400

    Complete ArrayListMultiset.java implementation.

---
+++ src/ArrayListMultiset.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayListMultiset {
+    private ArrayList<Integer> list;
+
+    public ArrayListMultiset() {
+        list = new ArrayList<>();
+    }
+
+    public void add(Integer item) {
+        list.add(item);
+    }
+
+    public void remove(Integer item) {
+        if (list.contains(item)) {
+            list.remove(item);
+        }
+    }
+
+    public boolean contains(Integer item) {
+        return list.contains(item);
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public Integer count(Integer item) {
+        return Collections.frequency(list, item);
+    }
+
+    public int size() {
+        return list.size();
+    }
+}
